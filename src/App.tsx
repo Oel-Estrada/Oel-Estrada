37ef2c612e0847a98cf308a032d0926f7174c89c
Oel Estrada <oelestradacampos@gmail.com>
2026-01-11 22:24:52 +0300

    fix: add space in self-closing tag for ComingSoon component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import '@/styles/App.css';
 import ComingSoon from "@/pages/ComingSoon/ComingSoon.tsx";
 
 function App() {
-    return <ComingSoon/>;
+    return <ComingSoon />;
 }
 
 export default App;
