9ddb501162be2d5c99416a9d5c6592fe023fc086
Oel Estrada <oelestradacampos@gmail.com>
2026-01-11 22:24:29 +0300

    fix: add spaces in self-closing tags in root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -6,24 +6,24 @@ export function Layout({ children }: {
     return (
         <html lang="en">
         <head>
-            <meta charSet="UTF-8"/>
+            <meta charSet="UTF-8" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
             />
             <title>Oel Estrada</title>
-            <Meta/>
-            <Links/>
+            <Meta />
+            <Links />
         </head>
         <body>
         {children}
-        <ScrollRestoration/>
-        <Scripts/>
+        <ScrollRestoration />
+        <Scripts />
         </body>
         </html>
     );
 }
 
 export default function Root() {
-    return <Outlet/>;
+    return <Outlet />;
 }
