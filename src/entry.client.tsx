e01f080cf4770f780388ff0c7889c6d1326af863
Oel Estrada <oelestradacampos@gmail.com>
2026-01-11 21:43:00 +0300

    feat: add entry point for client-side rendering with React and React Router

---
+++ src/entry.client.tsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { HydratedRouter } from 'react-router/dom';
+import '@/styles/index.css';
+
+import './i18n';
+
+ReactDOM.hydrateRoot(
+    document,
+    <React.StrictMode>
+        <Suspense fallback={<div>Loading...</div>}>
+            <HydratedRouter/>
+        </Suspense>
+    </React.StrictMode>,
+);
