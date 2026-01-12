0d49b5e16d01656765b17a0fca3837dcf222f031
Oel Estrada <oelestradacampos@gmail.com>
2026-01-13 01:10:41 +0300

    feat: update i18n import path for client-side internationalization support

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
 import '@/styles/index.css';
 
-import './i18n';
+import "@/i18n/client";
 
 ReactDOM.hydrateRoot(
     document,
