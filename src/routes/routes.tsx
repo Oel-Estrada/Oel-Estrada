8e2985456e0a3275180df902cd2500610477deb8
Oel Estrada <oelestradacampos@gmail.com>
2026-01-13 12:36:40 +0300

    refactor: update import paths and use RootRedirect for root route redirect

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,12 @@
-import { Navigate, type RouteObject } from "react-router-dom";
+import { type RouteObject } from "react-router-dom";
 import ComingSoon from "@/pages/ComingSoon/ComingSoon.tsx";
-import LangLayout from "@/routes/lang-layout.tsx";
-import i18n from "@/i18n/client";
+import LangLayout from "@/routes/LangLayout.tsx";
+import RootRedirect from "@/routes/RootRedirect.tsx";
 
 export const routes: RouteObject[] = [
     {
         path: "/",
-        element: <Navigate to={`/${i18n.language}`} replace />,
+        Component: RootRedirect,
     },
     {
         path: "/:lang",
