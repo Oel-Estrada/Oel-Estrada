e8b53660a5a6298ffdafafd581a7cd46a35d35f6
Oel Estrada <oelestradacampos@gmail.com>
2026-01-13 12:35:32 +0300

    feat: add RootRedirect component for default language-based redirection

---
+++ src/routes/RootRedirect.tsx
@@ -0,0 +1,11 @@
+import { useTranslation } from "react-i18next";
+import { Navigate } from "react-router";
+
+/**
+ * RootRedirect component that redirects to the default language route.
+ */
+export default function RootRedirect() {
+    const { i18n } = useTranslation();
+
+    return <Navigate to={`/${i18n.language}`} replace />;
+}
