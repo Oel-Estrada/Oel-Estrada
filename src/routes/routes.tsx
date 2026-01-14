aff5b621894dd34c8525e9e6269f9d3576c190d7
Oel Estrada <oelestradacampos@gmail.com>
2026-01-14 16:15:39 +0300

    refactor(routes): add `isDevelopment` flag to routes and filter logic for environment-specific routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,28 @@
-import { type RouteObject } from "react-router-dom";
+import { type RouteObject } from "react-router";
 import ComingSoon from "@/pages/ComingSoon/ComingSoon.tsx";
 import StyleGuide from "@/pages/StyleGuide/StyleGuide.tsx";
 import LangLayout from "@/routes/LangLayout.tsx";
 import RootRedirect from "@/routes/RootRedirect.tsx";
 import { isDev } from "@/env.ts";
 
-export const routes: RouteObject[] = [
+/**
+ * Extended RouteObject to include development-only routes.
+ */
+export type AppRouteObject = RouteObject & {
+    isDevelopment?: boolean;
+    children?: AppRouteObject[];
+};
+
+const routesConfig: AppRouteObject[] = [
     {
         path: "/",
         Component: RootRedirect,
     },
-    ...(isDev
-        ? [
-            {
-                path: "/style-guide",
-                Component: StyleGuide,
-            },
-        ]
-        : []),
+    {
+        path: "/style-guide",
+        Component: StyleGuide,
+        isDevelopment: true,
+    },
     {
         path: "/:lang",
         Component: LangLayout,
@@ -33,3 +38,22 @@ export const routes: RouteObject[] = [
         ],
     },
 ];
+
+/**
+ * Filter routes based on the environment.
+ * @param routes
+ */
+const filterRoutes = (routes: AppRouteObject[]): RouteObject[] =>
+    routes
+        .filter((route) => !route.isDevelopment || isDev)
+        .map((route) => {
+            const filteredRoute = { ...route };
+            if (filteredRoute.children) {
+                filteredRoute.children = filterRoutes(filteredRoute.children) as AppRouteObject[];
+            }
+
+            delete filteredRoute.isDevelopment;
+            return filteredRoute as RouteObject;
+        });
+
+export const routes: RouteObject[] = filterRoutes(routesConfig);
