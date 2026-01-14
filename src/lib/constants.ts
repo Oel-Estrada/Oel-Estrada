499c873f2ea7aa60f93e3fdc480c9156593d3a01
Oel Estrada <oelestradacampos@gmail.com>
2026-01-14 14:43:36 +0300

    refactor(constants): convert `ContactInfo` to a class with constructor and dynamic getters

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,30 +1,53 @@
-export interface ContactInfo {
+export class ContactInfo {
     phone: string;
     email: string;
     telegramUsername: string;
-    linkedInUsername: string;
+    linkedinUsername: string;
+    githubUsername?: string;
     website?: string;
-    whatsapp: string;
-    telegram: string;
-    linkedIn: string;
+
+    constructor(opts: {
+        phone: string;
+        email: string;
+        telegramUsername: string;
+        linkedinUsername: string;
+        githubUsername?: string;
+        website?: string;
+    }) {
+        this.phone = opts.phone;
+        this.email = opts.email;
+        this.telegramUsername = opts.telegramUsername;
+        this.linkedinUsername = opts.linkedinUsername;
+        this.githubUsername = opts.githubUsername;
+        this.website = opts.website;
+    }
+
+    get sanitizedPhone(): string {
+        return this.phone.replace(/[^\d]/g, "");
+    }
+
+    get whatsapp(): string {
+        return `https://wa.me/${this.sanitizedPhone}`;
+    }
+
+    get telegram(): string {
+        return `https://t.me/${this.telegramUsername}`;
+    }
+
+    get linkedin(): string {
+        return `https://www.linkedin.com/in/${this.linkedinUsername}`;
+    }
+
+    get github(): string | undefined {
+        return this.githubUsername ? `https://github.com/${this.githubUsername}` : undefined;
+    }
 }
 
-export const contactInfo: ContactInfo = {
+export const contactInfo = new ContactInfo({
     phone: "79955556081",
     email: "oelestradacampos@gmail.com",
     telegramUsername: "Oel_Estrada",
-    linkedInUsername: "oel-estrada",
+    linkedinUsername: "oel-estrada",
+    githubUsername: "Oel-Estrada",
     website: "https://www.oelestrada.ru",
-
-    get whatsapp() {
-        return `https://wa.me/${this.phone.replace(/[^\d]/g, "")}`;
-    },
-
-    get telegram() {
-        return `https://t.me/${this.telegramUsername}`;
-    },
-
-    get linkedIn() {
-        return `https://www.linkedin.com/in/${this.linkedInUsername}`;
-    }
-};
+});
