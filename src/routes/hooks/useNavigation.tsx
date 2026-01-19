a71e4d5c40af861a56eb7d7166e24147d3e9b73a
Oel Estrada <oelestradacampos@gmail.com>
2026-01-20 00:28:11 +0300

    feat(useNavigation): add custom hook for localized navigation support

---
+++ src/routes/hooks/useNavigation.tsx
@@ -0,0 +1,28 @@
+import { type To, useNavigate as useReactRouterNavigate } from 'react-router';
+
+import { useLocalizedPath } from '@/routes';
+
+import type { NavigateOptions } from 'react-router';
+
+type PathParamValue = string | number;
+type PathParams = Record<string, PathParamValue>;
+type AppNavigateOptions = NavigateOptions & { params?: PathParams };
+type AppNavigate = (to: To, options?: AppNavigateOptions) => Promise<void>;
+
+/**
+ * Custom hook to navigate to a specified route with localization support.
+ */
+export function useNavigation(): AppNavigate {
+    const navigate = useReactRouterNavigate();
+    const getLocalizedPath = useLocalizedPath();
+
+    return async (to, options) => {
+        const localizedTo = getLocalizedPath(to, options?.params);
+
+        const { params: _params, ...navOptions } = options ?? {};
+
+        await navigate(localizedTo, navOptions);
+    };
+}
+
+export type { AppNavigate, AppNavigateOptions };
