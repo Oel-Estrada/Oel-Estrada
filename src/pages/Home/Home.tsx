2faf6dd33b51ad6c13e6ee6ce88df6f13d5708d8
Oel Estrada <oelestradacampos@gmail.com>
2026-01-24 19:06:44 +0300

    feat(Home): create hero section layout with text and avatar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,20 @@ import type { JSX } from 'react';
  * @return {JSX.Element} The JSX structure for the Home component, displaying the Home Page.
  */
 function Home(): JSX.Element {
-    return <div>Home Page</div>;
+    return (
+        <section className="w-[90%] max-w-6xl mx-auto">
+            <div className="max-w-275 mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
+                    <div className="lg:col-span-7 flex flex-col gap-8">
+                        Text Information
+                    </div>
+                    <div className="lg:col-span-5 relative hidden lg:block">
+                        Avatar
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Home;
