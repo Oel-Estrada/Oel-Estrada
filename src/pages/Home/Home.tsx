39c3cfd7ba70821a708c73765de5065a2cc27056
Oel Estrada <oelestradacampos@gmail.com>
2026-01-25 17:22:36 +0300

    feat(Home): enhance hero section with translation support and update Badge text

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 import Badge from '@/components/Badge/Badge.tsx';
 
@@ -10,7 +10,7 @@ import type { JSX } from 'react';
  * @return {JSX.Element} The JSX structure for the Home component, displaying the Home Page.
  */
 function Home(): JSX.Element {
-    const { t } = useTranslation();
+    const { t } = useTranslation(['home', 'common']);
 
     return (
         <section className="w-[90%] max-w-6xl mx-auto mt-4">
@@ -18,16 +18,18 @@ function Home(): JSX.Element {
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
                     <div className="lg:col-span-7 flex flex-col gap-8">
                         <Badge
-                            text={t('status.availableForProjects')}
+                            text={t('common:status.availableForProjects')}
                             showPing
                             variant="primary"
                         />
                         <div className="flex flex-col gap-4">
                             <h1 className="text-5xl md:text-7xl font-black leading-[1.1] tracking-tight">
-                                Creando experiencias digitales de{' '}
-                                <span className="text-primary">
-                                    alto rendimiento
-                                </span>
+                                <Trans
+                                    i18nKey="home:hero.title"
+                                    components={{
+                                        1: <span className="text-primary" />,
+                                    }}
+                                />
                             </h1>
                         </div>
                     </div>
