38d5c386c42e6bd3ef2369515567353f17b5e3f1
Oel Estrada <oelestradacampos@gmail.com>
2026-01-25 16:54:24 +0300

    feat(Home): integrate Badge component for project availability notification

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import Badge from '@/components/Badge/Badge.tsx';
+
 import type { JSX } from 'react';
 
 /**
@@ -7,11 +9,15 @@ import type { JSX } from 'react';
  */
 function Home(): JSX.Element {
     return (
-        <section className="w-[90%] max-w-6xl mx-auto">
+        <section className="w-[90%] max-w-6xl mx-auto mt-4">
             <div className="max-w-275 mx-auto">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
                     <div className="lg:col-span-7 flex flex-col gap-8">
-                        Text Information
+                        <Badge
+                            text="Disponible para nuevos proyectos"
+                            showPing
+                            variant="primary"
+                        />
                     </div>
                     <div className="lg:col-span-5 relative hidden lg:block">
                         Avatar
