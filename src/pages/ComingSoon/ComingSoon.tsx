8d386f9cbc7da7b848b40dfe1db6a29400f0529a
Oel Estrada <oelestradacampos@gmail.com>
2026-01-29 11:51:19 +0300

    refactor(ComingSoon): simplify logo component imports and remove unused logo definitions

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -9,11 +9,7 @@ import { Trans, useTranslation } from 'react-i18next';
 
 import './ComingSoon.css';
 import Badge from '@/components/Badge/Badge.tsx';
-import JavaScriptLogo from '@/components/icons/JavaScriptLogo.tsx';
-import NextJsLogo from '@/components/icons/NextJsLogo.tsx';
-import ReactLogo from '@/components/icons/ReactLogo.tsx';
 import TelegramLogo from '@/components/icons/TelegramLogo.tsx';
-import TypeScriptLogo from '@/components/icons/TypeScriptLogo.tsx';
 import WhatsAppLogo from '@/components/icons/WhatsAppLogo.tsx';
 import Logo from '@/components/Logo/Logo.tsx';
 import MetaTags from '@/components/MetaTags.tsx';
@@ -21,44 +17,13 @@ import SelectLanguage from '@/components/Navbar/components/SelectLanguage.tsx';
 import { Button } from '@/components/ui/button.tsx';
 import { contactInfo } from '@/lib/constants.ts';
 import { cn } from '@/lib/utils.ts';
+import { logos } from '@/pages/ComingSoon/constants/data.ts';
 import {
     itemVariants,
     progressVariants,
 } from '@/pages/ComingSoon/constants/motion.ts';
 
-import type { ComponentType, JSX, SVGProps } from 'react';
-
-interface logos {
-    id: number;
-    Icon: ComponentType<SVGProps<SVGSVGElement>>;
-    className?: string;
-}
-
-const logos: logos[] = [
-    {
-        id: 0,
-        Icon: TypeScriptLogo,
-        className:
-            'w-24 h-24 bottom-[20%] right-[10%] opacity-5 floating-icon ',
-    },
-    {
-        id: 1,
-        Icon: JavaScriptLogo,
-        className:
-            'w-32 h-32 top-[25%] right-[20%] opacity-5 floating-icon-delayed ',
-    },
-    {
-        id: 2,
-        Icon: ReactLogo,
-        className:
-            'w-32 h-32 bottom-[30%] left-[15%] opacity-5 floating-icon-slow ',
-    },
-    {
-        id: 3,
-        Icon: NextJsLogo,
-        className: 'w-24 h-24 top-[15%] left-[10%] opacity-50 floating-icon ',
-    },
-];
+import type { JSX } from 'react';
 
 /**
  * "Coming Soon" page.
