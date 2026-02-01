d46c17bd90a4ec16a86641b936f096cdaf0081a3
Oel Estrada <oelestradacampos@gmail.com>
2026-02-01 13:11:28 +0300

    refactor: update NotFoundPage background opacity for improved visibility

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -11,7 +11,7 @@ function NotFoundPage(): JSX.Element {
     return (
         <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-6 text-center">
             NotFoundPage
-            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-150 h-150 bg-primary/5 rounded-full blur-[120px] pointer-events-none z-0"></div>
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-150 h-150 bg-primary/20 rounded-full blur-[120px] pointer-events-none z-0"></div>
         </div>
     );
 }
