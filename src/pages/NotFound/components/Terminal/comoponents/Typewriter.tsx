7ecd73c13fdad5250c6675a3e28c5e1d20dd426d
Oel Estrada <oelestradacampos@gmail.com>
2026-02-02 23:56:24 +0300

    feat: add Typewriter component for animated text display

---
+++ src/pages/NotFound/components/Terminal/comoponents/Typewriter.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+import { animate } from 'motion';
+import type { JSX } from 'react';
+
+interface TypewriterProps {
+    text: string;
+    duration?: number;
+}
+
+/**
+ * Typewriter component to display text with a typewriter animation effect.
+ * @param {TypewriterProps} props - The component props.
+ *
+ * @param {string} [props.text='Hola mundo'] - The text to display.
+ * @param {number} [props.duration=3] - The duration of the animation in seconds.
+ *
+ * @returns {JSX.Element} The Typewriter component.
+ */
+function Typewriter({ text, duration = 0.8 }: TypewriterProps): JSX.Element {
+    const [count, setCount] = useState(0);
+
+    /**
+     * Effect to animate the typewriter effect whenever the text or duration changes.
+     */
+    useEffect(() => {
+        setCount(0);
+
+        const controls = animate(0, text.length, {
+            duration,
+            ease: 'linear',
+            onUpdate: (latest: number) => setCount(Math.floor(latest)),
+        });
+
+        return () => controls.stop();
+    }, [text, duration]);
+
+    return <>{text.slice(0, count)}</>;
+}
+
+export default Typewriter;
